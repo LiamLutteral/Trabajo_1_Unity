174ff05b59d4368f7b2d30a55da911f6eda9a224
LiamLutteral <49008662@est.ort.edu.ar>
2025-03-17 10:30:34 -0300

    variables creadas
    hice cosas

--- dani.cs
+++ dani.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class dani : MonoBehaviour
 {
+    int edadUsuario;
+    float sueldousuario;
+    string domicilio;
+    bool estaAprobado;
     // Start is called before the first frame update
     void Start()
     {
